4c2c768ccb076d52ca4d1673b7b3b414b3198483
Ifedayo Bolade <pearlyrose3@gmail.com>
2026-07-03 18:27:01 +0100

    Initial commit

--- sample-java/build.gradle.kts
+++ sample-java/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "ifedayo.bolade.sample_java"
         minSdk = 23
         targetSdk = 37
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
